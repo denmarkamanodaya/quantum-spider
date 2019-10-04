68671ad5e299abc4e7933f68fa6cf3496037f821
Denmark Amano Daya <denmarkamanodaya.fs360@gmail.com>
2019-10-04 10:08:47 +0800

    test

--- app/spiders/Casper/Motors/merlin-car-auctions/main.js
+++ app/spiders/Casper/Motors/merlin-car-auctions/main.js
@@ -42,6 +42,8 @@ var casper = qs.getCasper();
  */
 linkSpiderEventListeners();
 
+casper.userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36");
+
 casper
     .start("http://www.merlin.ie/carlist#/?pageSize=10&sortBy=make_asc")
     .then(function() {
